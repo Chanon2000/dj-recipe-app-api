b0383b4d2c4e0fe1bd35a1bd60df01b6e4dae557
Chanon <chanonmixmay2543@hotmail.com>
2022-11-04 13:34:26 +0700

    Remove Lecture Section 07

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,6 @@
 """
 Calculator functions
-""" # เขียน doc string เอาเพื่อให้รู้ว่า module นี้จะทำอะไร
+""" 
 
 def add(x ,y):
     """Add x and y and return result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,9 @@
-# file tests.py จะสร้าง auto เมื่อเราสร้าง app ใน Project
 """
 Sample tests
 """
-from django.test import SimpleTestCase # import test class ที่จะใช้
+from django.test import SimpleTestCase
 
-from app import calc # impot module ที่ต้องการ test
+from app import calc
 
 
 class CalcTests(SimpleTestCase):
@@ -14,16 +13,10 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         
-        self.assertEqual(res, 11) # เพื่อ check ouput
-
-    # นี้คือ idea ของ TDD คือ เขียน test เพื่อขับเคลื่อน development (คือเขียน test นำ code)
-    # ขั้นตอนคือ
-    # 1. Write test for behaviour expected to see in code
-    # 2. Test fails (เพราะเรายังไม่ได้เขียน code)
-    # 3. Write code so test passes
+        self.assertEqual(res, 11) 
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res, 5) # คือเราคาดหวังจะเห็น 5 เป็น result ของ test
+        self.assertEqual(res, 5) 

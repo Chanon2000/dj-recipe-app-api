5638e8c02b9a73a2f7f0c066edb69751d99572d5
Chanon <chanonmixmay2543@hotmail.com>
2022-11-04 12:08:00 +0700

    se07: 28. Write a test using TDD

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 def add(x ,y):
     """Add x and y and return result."""
     return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return result."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -15,3 +15,15 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
         
         self.assertEqual(res, 11) # เพื่อ check ouput
+
+    # นี้คือ idea ของ TDD คือ เขียน test เพื่อขับเคลื่อน development (คือเขียน test นำ code)
+    # ขั้นตอนคือ
+    # 1. Write test for behaviour expected to see in code
+    # 2. Test fails (เพราะเรายังไม่ได้เขียน code)
+    # 3. Write code so test passes
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5) # คือเราคาดหวังจะเห็น 5 เป็น result ของ test

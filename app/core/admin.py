1cc488c3e89cda7405c0c6d8349028ca4949a2b1
Chanon <chanonmixmay2543@hotmail.com>
2022-11-05 16:16:20 +0700

    Remove Lecture Section 10

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,27 +2,19 @@
 Django admin customization.
 """
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin # เรียก UserAdmin ว่า BaseUserAdmin เพราะมันง่ายกว่าในการเรียก ทำให้ไม่งง
-# UserAdmin เป็น class ที่มาจาก Django
-from django.utils.translation import gettext_lazy as _ # ที่ไหนที่คุณใช้ _ (translation shortcut) มันจะ automatically translate the text
-# ซึ่ง _ เป็น standard ที่ Django อยากให้เราใช้ด้วยนะ
-# เนื่องจาก gettext_lazy มันร่วมกับ Django translation system ด้วย
-# โดยการทำงานของมันคือ จะทำให้ เมื่อเช่นคุณเปลี่ยนภาษาใน Django settings.py ทุกที่ที่คุณใส่ _ มันก็จะ translate เปลี่ยนภาษาให้คุณด้วยเลย แต่ถ้าคุณไม่เปลี่ยน settings อะไร มันก็จะไม่ translate อะไร
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-from core import models # import models ใน core app มา
+from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
-    ordering = ['id'] # ถ้าพิมพ์ key ผิดจาก ordering เป็น order มันก็จะใช้ default ordering ทันที
-    list_display = ['email', 'name'] # แสดง field ตามนี้
-    # จาก error ที่มันพลายามจะแสดง field ที่อยู่ใน default user model ของ django ซึ่งบาง field นั้น custom user model ของเรามันไม่มี เราเลยต้อง override fieldsets ให้แสดงแค่ field ที่เรามี
+    ordering = ['id']
+    list_display = ['email', 'name']
     fieldsets = (
-        # None คือจะไม่ใส่ title
-        # โดยมี 2 field คือ 'email', 'password'
         (None, {'fields': ('email', 'password')}),
         (
-            _('Permissions'), # _ ก็คือจะเรียก gettext_lazy นั้นแหละ
-            # ตรงนี้จะเป็น Permissions section ที่จะเป็นอีก section ใน page
+            _('Permissions'),
             {
                 'fields': (
                     'is_active',
@@ -31,14 +23,12 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        # Important dates section ที่มี last_login fields แสดงอยู่
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_login'] # ทำให้แก้ไม่ได้
+    readonly_fields = ['last_login']
     add_fieldsets = (
-        # section ที่ไม่มี title
         (None, {
-            'classes': ('wide',), # คล้ายๆกับ ใส่ css ให้ admin panel (ไม่ได้มีอะไรมาก)
+            'classes': ('wide',),
             'fields': (
                 'email',
                 'password1',
@@ -48,7 +38,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_staff',
                 'is_superuser',
             ),
-        }), # อย่าลืม ใส่ , ตรงนี้ด้วย เพราะนี้เป็น tuple
+        }),
     )
 
-admin.site.register(models.User, UserAdmin) # เพื่อบอกให้ใช้ UserAdmin ใน models.User (ถ้าไม่บอกมันก็จะใช้ default)
+admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -11,41 +11,35 @@ from django.test import Client
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
 
-    # สร้าง setup method เข้า class ซึ่ง setUp จะรันทุกครั้งในทุก test method ใน class นี้
-    def setUp(self): # ใน unittest lib นี้ เขาหน้าจะพลาดตอนตั้งชื่อแล้วไม่ได้แก้ เพราะเอาจริงๆชื่อว่า set_up ดีกว่า
+    def setUp(self):
         """Create user and client."""
-        self.client = Client() # คือ django test client เพื่อทำ http request
-        # docs: https://docs.djangoproject.com/en/3.2/topics/testing/tools/#overview-and-a-quick-example
+        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123',
         )
-        self.client.force_login(self.admin_user) # force the authentication ด้วย self.admin_user คนนี้
-        # ทำให้ทุก request ที่ทำจาก self.client จะ authenticated เป็น self.admin_user คนนี้
+        self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
             name='Test User'
         )
 
-    # สร้าง test method ชื่อ test_users_lists (หรือก็คือ unittest ของเรา)
+
     def test_users_lists(self):
         """Test that users are listed on page."""
-        url = reverse('admin:core_user_changelist') # reverse เพื่อเอา url
-        # 'admin:core_user_changelist' คือ syntax เพื่อกำหนดว่าเราจะเอา url อะไรจาก django admin ซึ่งในที่นี้คือเราจะเอา page ที่แสดง list ของ users ในระบบเรา
-        # docs: https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#reversing-admin-urls
-        res = self.client.get(url) # call http request
-        # ซึ่ง request ถูก authenticated แล้ว โดย user ที่อยู่ใน setUp method
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-        self.assertContains(res, self.user.name) # check ว่า page มี name ของ user อยู่มั้ย (เพราะหน้านั้นต้อง list users ทั้งหมดในระบบ)
-        self.assertContains(res, self.user.email) # เช่นเดียวกับ name
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
         """Test the edit user page works."""
-        url = reverse('admin:core_user_change', args=[self.user.id]) # เนื่องจากเราเข้าหน้า edit เลยต้องระบุ id ของ user ที่จะเข้าด้วย (ดู url จาก docs)
+        url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
-        self.assertEqual(res.status_code, 200) # check ว่า page load success ด้วย status_code=200
+        self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
         """Test the create user page works."""

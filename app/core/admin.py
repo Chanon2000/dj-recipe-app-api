1ac6126828824ec7fa6e14a60d8af8acb7c6f313
Chanon <chanonmixmay2543@hotmail.com>
2022-11-05 15:04:57 +0700

    se10: 55. Make Django admin list users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin # เรียก UserAdmin ว่า BaseUserAdmin เพราะมันง่ายกว่าในการเรียก ทำให้ไม่งง
+# UserAdmin เป็น class ที่มาจาก Django
+from core import models # import models ใน core app มา
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id'] # ถ้าพิมพ์ key ผิดจาก ordering เป็น order มันก็จะใช้ default ordering ทันที
+    list_display = ['email', 'name'] # แสดง field ตามนี้
+
+admin.site.register(models.User, UserAdmin) # เพื่อบอกให้ใช้ UserAdmin ใน models.User (ถ้าไม่บอกมันก็จะใช้ default)

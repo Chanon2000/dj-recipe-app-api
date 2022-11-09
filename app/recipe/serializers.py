3e94fb2e5652613b6e7a7d47155e08bd43a9253d
Chanon <chanonmixmay2543@hotmail.com>
2022-11-09 14:52:01 +0700

    se14: 92. Implement tag listing API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,10 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
@@ -18,3 +21,11 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,17 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import ( # import หลายอันให้ใส่เป็น tuple
+    viewsets,
+    mixins, # คือสิ่งที่สามารถใส่เข้าไปใน view เพื่อเพิ่ม functionality
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -29,3 +35,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+# เราจะต้องมี basic CRUD implementation
+class TagViewSet(mixins.ListModelMixin, # เป็น mixins เพื่อเพิ่ม listing functionality ให้กับการ listing models
+                 viewsets.GenericViewSet): # viewsets.GenericViewSet ให้คุณสามารถใส่mixinsได้ (เหมือนกับ GenericViewSet มี basic func ต่างๆให้ จากนั้นเอา mixins มาเพิ่มความสามารถให้กับ basic function ต่างๆเหล่านั้น)
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    # property พวกนี้ พิมพ์สะกดผิดทีนิหา bug ค่อนข้างยากเลย
+
+    # override get_queryset method
+    def get_queryset(self): # เพื่อให้ตอน get เอาเฉพาะ tags ของ user ที่ login เท่านั้น
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')

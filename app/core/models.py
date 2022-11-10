3b16aa51675bf46265e4c7a2f8de50c30ced4c4b
Chanon <chanonmixmay2543@hotmail.com>
2022-11-10 08:20:29 +0700

    se15: 105. Add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -54,6 +54,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient') # 1 recipe มีหลาย ingredients ซึ่ง 1 ingredients ก็มีหลาย recipe
 
     def __str__(self):
         return self.title
@@ -61,6 +62,17 @@ class Recipe(models.Model):
 
 class Tag(models.Model):
     """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
